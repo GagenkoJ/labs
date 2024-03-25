5405847cec6e3b72a9f45822db7521d07f669cb4
GagenkoJ <114720565+GagenkoJ@users.noreply.github.com>
2024-03-25 16:57:39 +0200

    Add files via upload

---
+++ Laba_2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿List<int> numList = new List<int>{1, 10, 20};
+List<int> sortList = new List<int>{};
+
+foreach (int testNum in numList)
+{
+    if (testNum < 19 && testNum >= 1)
+    {
+        sortList.Add(testNum);
+        Console.WriteLine(testNum + " ");
+    }
+}
