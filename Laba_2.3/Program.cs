369490ca657aadd79b3be29fc514351da81fd0e1
GagenkoJ <114720565+GagenkoJ@users.noreply.github.com>
2024-03-25 17:25:52 +0200

    Add files via upload

---
+++ Laba_2.3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+class HelloWorld {
+    static void Main() {
+        int[] myArray = new int[5];
+        FunctionsOfArray arrayClass = new FunctionsOfArray();
+        arrayClass.FillArray(myArray);
+        arrayClass.PrintArray(myArray);
+        arrayClass.MaxAndMinElemet(myArray);
+    }
+}
+
+public class FunctionsOfArray
+{
+    public void FillArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i;
+        }
+    }
+
+    public void PrintArray(int[] array)
+    {
+        foreach (var number in array)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void MaxAndMinElemet(int[] array)
+    {
+        int maxElement = array[0], minElement = array[0];
+        int maxIndex, minIndex;
+        for (int i = 1; i < array.Length; i++)
+        {
+            maxElement = array[i] > maxElement ? array[i] : maxElement;
+            minElement = array[i] < minElement ? array[i] : minElement;
+        }
+
+        Console.Write("max:" +  maxElement);
+        Console.Write("min:" +  minElement);
+
+    }
+    
+}
