0e674513b9a94b7cb3170cd2bf838a105b0e994b
GagenkoJ <114720565+GagenkoJ@users.noreply.github.com>
2024-03-25 19:59:23 +0200

    Add files via upload

---
+++ Laba_2.4/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] array = new int[18];
+int m = 7;
+//FillArray
+int count = -9;
+for (int i = 0; i < array.Length; i++, count++)
+{
+    array[i] = count;
+}
+
+List<int> numList = new List<int>{};
+foreach (int testNum in array)
+{
+    if (Math.Abs(testNum) > m)
+    {
+        numList.Add(testNum);
+    }
+}
+
+Console.WriteLine("Число: " + m);
+Console.WriteLine("1 масив: ");
+foreach (int num in array)
+{
+    Console.Write(num + " ");
+}
+
+Console.WriteLine();
+Console.WriteLine("2 масив");
+foreach (int num in numList)
+{
+    Console.Write(num + " ");
+}
